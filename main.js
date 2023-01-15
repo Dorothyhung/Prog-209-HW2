6d465387e6b98a28959872911c0aa89c1f7896ff
Dorothyhung <dorothyh0602@gmail.com>
2023-01-14 23:24:40 -0800

    create average program

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+let num1 = document.getElementById("num1").value
+
+
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    document.getElementById("button1").addEventListener("click", function () {
+    document.getElementById("button").addEventListener("click", function () {
 
-        document.getElementById("textbox1").value = "success";
+        document.getElementById("textbox1").value = (parseInt(num1+num2+num3))/3;
     });
 });
 
