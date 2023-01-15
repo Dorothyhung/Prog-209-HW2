347a7add967b060b5505ab2a32aa6253e82260e5
Dorothyhung <dorothyh0602@gmail.com>
2023-01-15 15:31:54 -0800

    limit user input to numbers, round result to 2 dec places, change button font color

--- main.js
+++ main.js
@@ -4,10 +4,11 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
     document.getElementById("button").addEventListener("click", function () {
-        let num1=parseInt(document.getElementById("num1").value);
-        let num2=parseInt(document.getElementById("num2").value);
-        let num3=parseInt(document.getElementById("num3").value);
-        document.getElementById("textbox1").value = (num1+num2+num3)/3;
+        let num1=Number(document.getElementById("num1").value);
+        let num2=Number(document.getElementById("num2").value);
+        let num3=Number(document.getElementById("num3").value);
+        document.getElementById("textbox1").value = ((num1+num2+num3)/3).toFixed(2);
+        document.getElementById("textbox1").style.backgroundColor="lightgreen";
     });
 });
 
