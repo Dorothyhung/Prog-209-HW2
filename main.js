ac8642000e7166f77ecf081ddeb73a97d11b3480
Dorothyhung <dorothyh0602@gmail.com>
2023-01-15 12:40:54 -0800

    add fonts and color

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-let num1 = document.getElementById("num1").value
+
 
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
     document.getElementById("button").addEventListener("click", function () {
-
-        document.getElementById("textbox1").value = (parseInt(num1+num2+num3))/3;
+        let num1=parseInt(document.getElementById("num1").value);
+        let num2=parseInt(document.getElementById("num2").value);
+        let num3=parseInt(document.getElementById("num3").value);
+        document.getElementById("textbox1").value = (num1+num2+num3)/3;
     });
 });
 
