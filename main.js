9e9dcf259fc3c0012be5c319c79a312b941eb7b2
Dorothyhung <dorothyh0602@gmail.com>
2023-01-15 21:34:46 -0800

    add copy to clipboard function, adjust rounding function

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-
-
-
 document.addEventListener("DOMContentLoaded", function (event) {
 
     document.getElementById("button").addEventListener("click", function () {
         let num1=Number(document.getElementById("num1").value);
         let num2=Number(document.getElementById("num2").value);
         let num3=Number(document.getElementById("num3").value);
-        document.getElementById("textbox1").value = ((num1+num2+num3)/3).toFixed(2);
-        document.getElementById("textbox1").style.backgroundColor="lightgreen";
+        var avg=Math.round((((num1+num2+num3)/3) + Number.EPSILON) * 100) / 100
+        document.getElementById("textbox1").value = avg;
     });
 });
 
+function copyText() {
+    var result=document.getElementById("textbox1");
+    result.select();
+    navigator.clipboard.writeText(result.value);
+    document.getElementById("textbox1").style.backgroundColor="lightgreen";
+}
